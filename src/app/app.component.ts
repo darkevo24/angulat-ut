8f3f8d869065fd5d202c90cabc58c21dcc1f7253
darkevo2424 <timotius.albert@luxpmsoft.com>
2022-05-06 06:34:15 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import wordsCount from 'words-count';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http:HttpClient) {
+  }
+  baseUrl = "https://random-data-api.com/api/restaurant/random_restaurant"
   title = 'UT';
+  api = new FormControl("");
+  name = new FormControl('');
+  res:any;
+  data:any;
+  input:any;
+  json:any;
+  address:any;
+  desc:any;
+  name2:any;
+  phone:any;
+  update(){
+    this.name.setValue("Timotius");
+  }
+	async GetAll() {
+		this.res = await fetch(this.baseUrl);
+    this.data = await this.res.json();
+    console.log(this.data);
+    this.address = wordsCount(this.data.address) ;
+    this.desc = wordsCount(this.data.description) ;
+    this.name2 = wordsCount(this.data.name) ;
+    this.phone = wordsCount(this.data.phone_number) ;
+	}
 }
